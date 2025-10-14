4f3d2043451d1f96bee148a69f10c5f3c5e4a089
Ramez Ehab <ramezehab2@gmail.com>
2025-10-14 17:53:44 +0300

    Initialize Project using React, Typescript & Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import icon from '@/assets/react.svg'
+const App = () => {
+  return (
+    <div className="bg-black w-screen h-screen text-white">
+      <img src={icon} alt="React logo" />
+    </div>
+  )
+}
+
+export default App
